67f23297498d18a71c6034aecfdd212a7be49c4f
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-12-26 01:06:33 +0100

    Removed debug message

--- drivers/spi/spi-omap2-mcspi.c
+++ drivers/spi/spi-omap2-mcspi.c
@@ -1298,7 +1298,6 @@ static int omap2_mcspi_probe(struct platform_device *pdev)
 		dev_dbg(&pdev->dev, "master allocation failed\n");
 		return -ENOMEM;
 	}
-        printk(KERN_ERR " DEBUG: omap2_mcspi_probe()");
 
 	/* the spi->mode bits understood by this driver: */
 	master->mode_bits = SPI_CPOL | SPI_CPHA | SPI_CS_HIGH;
