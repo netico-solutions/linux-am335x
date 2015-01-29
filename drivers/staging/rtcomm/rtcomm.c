5bf582f976c8df5f87d51b6db99c9caa5be54b09
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2015-01-29 09:47:34 +0100

    UP

--- drivers/staging/rtcomm/rtcomm.c
+++ drivers/staging/rtcomm/rtcomm.c
@@ -60,7 +60,14 @@
                                 ## __VA_ARGS__);                        \
                 }                                                       \
         } while (0)
-        
+
+
+struct acqunity_sample
+{
+    uint32_t                    channel[3];
+    uint32_t                    timestamp;
+};
+
 struct rtcomm_state 
 {
         struct spi_transfer     spi_transfer;
@@ -71,151 +78,36 @@ struct rtcomm_state
         bool                    is_busy;
 };
 
-struct acqunity_sample
-{
-    uint32_t                    channel[3];
-    uint32_t                    timestamp;
-};
-
-struct buff
-{
-    struct acqunity_sample *    storage;
-}
-
 struct ppbuff
 {
-    wait_queue_head_t           wait;
-    struct buff                 buffer[2];
-    struct buff *               consumer;
-    struct buff *               producer;
-    uint32_t                    size;
-    uint32_t                    lock_count;
-}
-
+        wait_queue_head_t       wait;
+        uint32_t                size;
+        uint32_t                count;
+        struct acqunity_sample * buffer;
+};
 
 static int rtcomm_open(struct inode * inode, struct file * fd);
 static int rtcomm_release(struct inode * inode, struct file * fd);
-static long rtcomm_ioctl(struct file * fd, unsigned int , unsigned long);
 static ssize_t rtcomm_read(struct file * fd, char __user *, size_t, loff_t *);
-
-static int ppbuff_init(struct ppbuff * ppbuff, uint32_t size)
-{
-        RTCOMM_DBG("init PPBUFF: %p, size: %d\n", ppbuff, size);
-        ppbuff->consumer = &ppbuff->buffer[0];
-        ppbuff->producer = &ppbuff->buffer[1];
-        ppbuff->size     = size;
-        ppbuff->lock_count = 0;
-        
-        RTCOMM_DBG("init PPBUFF consumer: %p\n", ppbuff->consumer);
-        RTCOMM_DBG("init PPBUFF producer: %p\n", ppbuff->producer);
-        ppbuff->consumer->storage = kzalloc(
-                sizeof(struct acqunity_sample) * size, GFP_KERNEL);
-        
-        RTCOMM_DBG("init PPBUFF consumer storage: %p\n", ppbuff->consumer->storage);
-        
-        if (ppbuff->consumer->storage == NULL) {
-                return (-ENOMEM);
-        }
-        ppbuff->producer->storage = kzalloc(
-                sizeof(struct acqunity_sample) * size, GFP_KERNEL);
-        
-        RTCOMM_DBG("init PPBUFF producer storage: %p\n", ppbuff->producer->storage);
-        
-        if (ppbuff->consumer->storage == NULL) {
-                return (-ENOMEM);
-        }
-        init_waitqueue_head((&ppbuff->wait);
-        
-        return (0);
-}
-
-static void ppbuff_term(struct ppbuff * ppbuff)
-{
-        RTCOMM_DBG("term PPBUFF: %p\n", ppbuff);
-        kfree(ppbuff->consumer->storage);
-        kfree(ppbuff->producer->storage);
-}
-
-static void ppbuff_swap(struct ppbuff * ppbuff)
-{
-        struct buff *           tmp;
-        
-        RTCOMM_DBG("swap PPBUFF: %p\n", ppbuff);
-
-        ppbuff->lock_count++;
-
-        tmp = ppbuff->consumer;
-        ppbuff->consumer = ppbuff->producer;
-        ppbuff->producer = tmp;
-        wake_up_interruptible(&ppbuff->wait);
-}
-
-static void * ppbuff_get_producer_storage(struct ppbuff * ppbuff)
-{
-        if (!ppbuff->lock_count) {
-                return (ppbuff->producer->storage);
-        } else {
-                return (NULL);
-        }
-}
-
-static void * ppbuff_get_consumer_storage(struct ppbuff * ppbuff)
-{
-        wait_event_interruptible(ppbuff->wait, ppbuff->lock_count != 0);
-        ppbuff->lock_count--;
-        
-        return (ppbuff->consumer->storage);
-}
-
-static uint32_t ppbuff_size(struct ppbuff * ppbuff)
-{
-        return (sizeof(struct acqunity_sample) * ppbuff->size);
-}
-
-
-
-static void transfer_done(void * arg)
-{
-        ppbuff_swap(&g_ppbuff);
-}
-
-static irqreturn_t trigger_notify_handler(int irq, void * p)
-{
-        unsigned long           flags;
-        unsigned int            chip_id_no;
-        void *                  storage;
-
-        disable_irq_nosync(irq);
-        storage = ppbuff_get_producer_storage(&g_ppbuff);
-        
-        if (storage) {
-                memset(&g_rtcomm_state.spi_transfer, 0, sizeof(g_rtcomm_state.spi_transfer));
-                g_rtcomm_state.spi_transfer.rx_buf = storage;
-                g_rtcomm_state.spi_transfer.len    = ppbuff_size(&g_ppbuff);
-                
-                spi_message_init(&g_rtcomm_state.spi_message);
-                spi_message_add_tail(&g_rtcomm_state.spi_transfer, &g_rtcomm_state.spi_message);
-                g_rtcomm_state.spi_message.complete = transfer_done;
-                g_rtcomm_state.spi_message.context  = NULL;
-                spi_async_locked(&g_spi, &g_rtcomm_state.spi_message);
-        }
-        enable_irq(irq);
-
-        return (IRQ_HANDLED);
-}
+static struct ppbuff * ppbuff_init(uint32_t size);
+static void ppbuff_term(struct ppbuff * ppbuff);
+static void ppbuff_producer_done(struct ppbuff * ppbuff);
+static void * ppbuff_get_producer_storage(struct ppbuff * ppbuff);
+static void * ppbuff_get_consumer_storage(struct ppbuff * ppbuff);
+static uint32_t ppbuff_size(struct ppbuff * ppbuff);
 
 /*--  Module parameters  ----------------------------------------------------*/
-static int g_bus_id = 0;
+static int g_bus_id = 1;
 module_param(g_bus_id, int, S_IRUGO);
 MODULE_PARM_DESC(g_bus_is, "SPI bus ID");
 
-static int g_notify;
+static int g_notify = 55;
 module_param(g_notify, int, S_IRUGO);
-MODULE_PARM_DESC(g_notify, "real-time notification GPIO pin");
+MODULE_PARM_DESC(g_notify, "notification GPIO pin");
 
 static int g_buff_size = 1024;
 module_param(g_buff_size, int, S_IRUGO);
-MODULE_PARM_DESC(g_notify, "real-time buffer size");
+MODULE_PARM_DESC(g_buff_size, "buffer size");
 
 
 static const struct file_operations g_rtcomm_fops = {
@@ -233,7 +125,121 @@ static struct miscdevice        g_rtcomm_miscdev = {
 
 static struct rtcomm_state      g_rtcomm_state;
 static struct spi_device *      g_spi;
-static struct ppbuff            g_ppbuff;
+static struct ppbuff *          g_ppbuff;
+
+static struct ppbuff * ppbuff_init(uint32_t size)
+{
+        struct ppbuff * ppbuff;
+
+        ppbuff = kmalloc(sizeof(struct ppbuff), GFP_KERNEL);
+        RTCOMM_DBG("init PPBUFF: %p, size: %d\n", ppbuff, size);
+
+        if (ppbuff == NULL) {
+                RTCOMM_ERR("failed to create PPBUFF\n");
+
+                return (NULL);
+        }
+        ppbuff->size  = size;
+        ppbuff->count = 0;
+        init_waitqueue_head(&ppbuff->wait);
+
+        ppbuff->buffer = kmalloc(sizeof(struct acqunity_sample) * size,
+                GFP_DMA | GFP_KERNEL);
+        RTCOMM_DBG("storage PPBUFF: %p\n", ppbuff->buffer);
+
+        if (ppbuff->buffer == NULL) {
+                RTCOMM_ERR("failed to allocate PPBUFF storage\n");
+                kfree(ppbuff);
+
+                return (NULL);
+        }
+        
+        return (ppbuff);
+}
+
+static void ppbuff_term(struct ppbuff * ppbuff)
+{
+        RTCOMM_DBG("term PPBUFF: %p\n", ppbuff);
+
+        if (ppbuff) {
+                kfree(ppbuff->buffer);
+                kfree(ppbuff);
+        }
+}
+
+static void ppbuff_producer_done(struct ppbuff * ppbuff)
+{
+        RTCOMM_DBG("done PPBUFF: %p, count: %d\n", ppbuff, ppbuff->count);
+
+        ppbuff->count = 1;
+        wake_up_interruptible(&ppbuff->wait);
+}
+
+static void ppbuff_consumer_done(struct ppbuff * ppbuff)
+{
+        ppbuff->count = 0;
+}
+
+static void * ppbuff_get_producer_storage(struct ppbuff * ppbuff)
+{
+        RTCOMM_DBG("get producer storage PPBUFF: %p, count %d\n", ppbuff, ppbuff->count);
+
+        if (ppbuff->count == 0) {
+                return (ppbuff->buffer);
+        } else {
+                return (NULL);
+        }
+}
+
+static void * ppbuff_get_consumer_storage(struct ppbuff * ppbuff)
+{
+        wait_event_interruptible(ppbuff->wait, ppbuff->count != 0);
+        
+        return (ppbuff->buffer);
+}
+
+static uint32_t ppbuff_size(struct ppbuff * ppbuff)
+{
+        return (sizeof(struct acqunity_sample) * ppbuff->size);
+}
+
+
+
+static void transfer_done(void * arg)
+{
+        ppbuff_producer_done(g_ppbuff);
+}
+
+static irqreturn_t trigger_notify_handler(int irq, void * p)
+{
+        void *                  storage;
+
+        disable_irq_nosync(irq);
+        #if 0
+        storage = ppbuff_get_producer_storage(g_ppbuff);
+        
+        if (storage) {
+                memset(&g_rtcomm_state.spi_transfer, 0, sizeof(g_rtcomm_state.spi_transfer));
+                g_rtcomm_state.spi_transfer.rx_buf = storage;
+                g_rtcomm_state.spi_transfer.len    = ppbuff_size(g_ppbuff);
+                
+                spi_message_init(&g_rtcomm_state.spi_message);
+                spi_message_add_tail(&g_rtcomm_state.spi_transfer, &g_rtcomm_state.spi_message);
+                g_rtcomm_state.spi_message.complete = transfer_done;
+                g_rtcomm_state.spi_message.context  = NULL;
+                spi_async_locked(g_spi, &g_rtcomm_state.spi_message);
+        } else {
+                RTCOMM_ERR("skipped read!\n");
+        }
+        #else
+        ppbuff_producer_done(g_ppbuff);
+        #endif
+        enable_irq(irq);
+
+        return (IRQ_HANDLED);
+}
+
+
 
 static int rtcomm_open(struct inode * inode, struct file * fd)
 {
@@ -248,9 +254,9 @@ static int rtcomm_open(struct inode * inode, struct file * fd)
         g_rtcomm_state.is_busy = true;
         fd->private_data = &g_rtcomm_state;
         
-        g_spi.bits_per_word = 8;
-        g_spi.mode          = SPI_MODE_1;
-        g_spi.max_speed_hz  = 10000000ul;
+        g_spi->bits_per_word = 8;
+        g_spi->mode          = SPI_MODE_1;
+        g_spi->max_speed_hz  = 10000000ul;
         ret = spi_setup(g_spi);
             
         if (ret) {
@@ -259,10 +265,10 @@ static int rtcomm_open(struct inode * inode, struct file * fd)
                 return (ret);
         }
         spi_bus_lock(g_spi->master);
-        g_ads1256_state.is_spi_locked = true;
-        ret = ppbuff_init(&g_ppbuff, g_buff_size);
+        g_rtcomm_state.is_spi_locked = true;
+        g_ppbuff = ppbuff_init(g_buff_size);
         
-        if (ret) {
+        if (g_ppbuff == NULL) {
                 RTCOMM_ERR("ppbuff_init() init failed: %d\n", ret);
                 spi_bus_unlock(g_spi->master);
                 
@@ -290,10 +296,32 @@ static int rtcomm_open(struct inode * inode, struct file * fd)
         }
         g_rtcomm_state.is_isr_init = true;
 
+        {
+            uint32_t    idx;
+            uint8_t buffer[32];
+            uint8_t received[32];
+            
+            memset(buffer, 0xa1, sizeof(buffer));
+            memset(&g_rtcomm_state.spi_transfer, 0, sizeof(g_rtcomm_state.spi_transfer));
+            g_rtcomm_state.spi_transfer.tx_buf = buffer;
+            g_rtcomm_state.spi_transfer.rx_buf = received;
+            g_rtcomm_state.spi_transfer.len    = sizeof(buffer);
+            
+            spi_message_init(&g_rtcomm_state.spi_message);
+            spi_message_add_tail(&g_rtcomm_state.spi_transfer, &g_rtcomm_state.spi_message);
+            g_rtcomm_state.spi_message.complete = NULL;
+            g_rtcomm_state.spi_message.context  = NULL;
+            spi_sync_locked(g_spi, &g_rtcomm_state.spi_message);
+            
+            for (idx = 0; idx < sizeof(received); idx++) {
+                RTCOMM_NOT("idx: %d = %d\n", idx, received[idx]);
+            }
+        }
+
         return (0);
 fail_gpio_isr_request:        
 fail_gpio_isr_map_request:
-        ppbuff_term(&g_ppbuff);
+        ppbuff_term(g_ppbuff);
         g_rtcomm_state.is_busy = false;
         spi_bus_unlock(g_spi->master);
         
@@ -313,10 +341,10 @@ static int rtcomm_release(struct inode * inode, struct file * fd)
         disable_irq_nosync(gpio_to_irq(g_notify));
         free_irq(gpio_to_irq(g_notify), NULL);
         spi_bus_unlock(g_spi->master);
-        ppbuff_term(&g_ppbuff);
-        g_ads1256_state.is_spi_locked = false;
-        g_rtcomm_state.is_isr_init    = false;
-        g_rtcomm_state.is_busy        = false;
+        ppbuff_term(g_ppbuff);
+        g_rtcomm_state.is_spi_locked = false;
+        g_rtcomm_state.is_isr_init   = false;
+        g_rtcomm_state.is_busy       = false;
 
         return (0);
 }
@@ -326,22 +354,33 @@ static int rtcomm_release(struct inode * inode, struct file * fd)
 static ssize_t rtcomm_read(struct file * fd, char __user * buff, size_t count, 
                 loff_t * off)
 {
-        struct rtcomm_state *   state = fd->private_data;
-        ssize_t                 ret;
         void *                  storage;
 
-        if (count > ppbuff_size(&g_ppbuff)) {
-                count = ppbuff_size(&g_ppbuff);
+        RTCOMM_DBG("read: size %d\n", count);
+
+        if (count > ppbuff_size(g_ppbuff)) {
+                count = ppbuff_size(g_ppbuff);
         }
         storage = ppbuff_get_consumer_storage(g_ppbuff);
         
         if (!storage) {
             return (-ENOMEM);
         }
+        memset(&g_rtcomm_state.spi_transfer, 0, sizeof(g_rtcomm_state.spi_transfer));
+        g_rtcomm_state.spi_transfer.rx_buf = storage;
+        g_rtcomm_state.spi_transfer.len    = ppbuff_size(g_ppbuff);
         
-        if (copy_to_user(buff, storage, sizeof(struct acqunity_sample) * count)) {
+        spi_message_init(&g_rtcomm_state.spi_message);
+        spi_message_add_tail(&g_rtcomm_state.spi_transfer, &g_rtcomm_state.spi_message);
+        g_rtcomm_state.spi_message.complete = NULL;
+        g_rtcomm_state.spi_message.context  = NULL;
+        spi_sync_locked(g_spi, &g_rtcomm_state.spi_message);
+        RTCOMM_DBG("read copy: size: %d\n", count);
+        
+        if (copy_to_user(buff, storage, count)) {
             return (-EFAULT);
         }
+        ppbuff_consumer_done(g_ppbuff);
         *off += count;
         
         return (count);
@@ -349,7 +388,7 @@ static ssize_t rtcomm_read(struct file * fd, char __user * buff, size_t count,
 
 
 
-static int __init ads1256_init(void)
+static int __init rtcomm_init(void)
 {
         char                    label[64];
         int                     ret;
@@ -360,6 +399,7 @@ static int __init ads1256_init(void)
                 .bus_num      = g_bus_id,
         };
         RTCOMM_NOT("registering RTCOMM device driver\n");
+        RTCOMM_NOT("BUILD: " __TIME__ "\n");
         RTCOMM_NOT("sizeof(struct acqunity_sample) = %d\n", 
                 sizeof(struct acqunity_sample));
 
@@ -378,6 +418,7 @@ static int __init ads1256_init(void)
                 return (-ENODEV);
         }
         sprintf(label, RTCOMM_NAME "-notify");
+        RTCOMM_INF("gpio name: %s\n", label);
         ret = gpio_request_one(g_notify, GPIOF_DIR_IN, label);
         
         if (ret) {
@@ -401,8 +442,10 @@ fail_gpio_request:
 
 
 
-static void __exit ads1256_exit(void)
+static void __exit rtcomm_exit(void)
 {
+        RTCOMM_NOT("deregistering\n");
+
         if (g_rtcomm_state.is_isr_init) {
                 disable_irq_nosync(gpio_to_irq(g_notify));
                 free_irq(gpio_to_irq(g_notify), NULL);
@@ -414,7 +457,7 @@ static void __exit ads1256_exit(void)
         
         if (g_spi) {
         
-                if (g_ads1256_state.is_spi_locked) {
+                if (g_rtcomm_state.is_spi_locked) {
                         spi_bus_unlock(g_spi->master);
                 }
                 spi_unregister_device(g_spi);
@@ -422,8 +465,8 @@ static void __exit ads1256_exit(void)
         misc_deregister(&g_rtcomm_miscdev);
 }
 
-module_init(ads1256_init);
-module_exit(ads1256_exit);
+module_init(rtcomm_init);
+module_exit(rtcomm_exit);
 MODULE_AUTHOR("Nenad Radulovic <nenad.b.radulovic@gmail.com>");
 MODULE_DESCRIPTION("Real-time communication driver");
 MODULE_LICENSE("GPL v2");
